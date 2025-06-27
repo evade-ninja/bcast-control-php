a4e1f84635640d2a70c3ab0935851005d3cd39d4
Travis Marks <travis.marks@byu.edu>
2025-06-26 18:46:59 -0600

    add readme and scripts

---
+++ run_capture.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sleep 10
+while true; do
+	/var/www/obs-capture.py 1
+	sleep 3
+done
---
+++ run_obs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sleep 10
+while true; do
+	/usr/bin/obs --disable-shutdown-check
+	sleep 3
+done
