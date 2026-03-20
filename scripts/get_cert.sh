229c121233d34aa0bc620d02d045229d290739dd
Travis Marks <travis.marks@byu.edu>
2026-03-20 10:23:08 -0600

    check correct location for cert

--- scripts/get_cert.sh
+++ scripts/get_cert.sh
@@ -6,19 +6,19 @@ if ! openssl x509 -in "$cert_path/$domain.crt" -noout -checkend "$threshold_seco
   echo "Certificate file expires within 30 days (or is expired): $cert_path. LETS RENEW!"
   wget -N $cert_www/$cert_name -P /tmp/
 
-  if [[ -d "$cert_path/$domain.crt" ]]; then
-  echo "New cert available"
-  mv -f /tmp/$cert_name $local_cert_path/
-  
-  #set permissions on certificate
-  chmod 640 $local_cert_path/$cert_name
+  if [[ -d "tmp/$cert_name" ]]
+  then
+    echo "New cert available"
+    mv -f /tmp/$cert_name $local_cert_path/
+    
+    #set permissions on certificate
+    chmod 640 $local_cert_path/$cert_name
 
-  #reload apache
-  systemctl reload apache2
-  
-  #remove temp file
-  rm /tmp/$cert_name
-
+    #reload apache
+    systemctl reload apache2
+  else
+    echo "No new cert"
   fi
+
 fi
 
