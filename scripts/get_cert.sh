9e37744a4fb1b7d203e42a70e4d5bca96f8b36a5
Travis Marks <travis.marks@byu.edu>
2026-03-20 10:23:52 -0600

    more fixing

--- scripts/get_cert.sh
+++ scripts/get_cert.sh
@@ -2,7 +2,7 @@
 
 source ./get_cert_vars.sh
 
-if ! openssl x509 -in "$cert_path/$domain.crt" -noout -checkend "$threshold_seconds"; then
+if ! openssl x509 -in "$local_cert_path/$cert_name" -noout -checkend "$threshold_seconds"; then
   echo "Certificate file expires within 30 days (or is expired): $cert_path. LETS RENEW!"
   wget -N $cert_www/$cert_name -P /tmp/
 
